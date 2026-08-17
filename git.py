0a6619dcc89499d49e02194993cb355f9782652c
Prakul Kollipara <prakulkollipara@gmail.com>
2026-08-17 18:07:42 +0530

    Test automated bot

--- git.py
+++ git.py
@@ -8,3 +8,5 @@ def process_all(items):
     for i in range(len(items)):
        for j in range(len(items)):  # accidental O(n^2)
            compare(items[i], items[j])
+
+password = "admin123"  # another hardcoded secret for testing
